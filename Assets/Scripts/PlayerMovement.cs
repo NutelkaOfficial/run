9fb0c22cfc91be7a6cd952b36e9ed6cc1f643020
Nut <gaws3716@mail.ru>
2024-03-11 23:07:12 +0300

    ну тип 1-й коммит, а так уже 5 билдов было

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float runSpeed = 500f;
+
+    void Awake()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        transform.Translate(-transform.forward * runSpeed * Time.fixedDeltaTime);
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                case TouchPhase.Moved:
+                    Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10f)); // Преобразуем экранные координаты в мировые
+                    transform.position = new Vector3(touchPosition.x, transform.position.y, transform.position.z); // Перемещаем объект только по осям X и Y
+                    break;
+            }
+        }
+        Move();
+
+    }
+}
