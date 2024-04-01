fd968b90f04dba5127699f72e433ed89fd798d2e
pukikaki123 <164878056+pukikaki123@users.noreply.github.com>
2024-04-01 13:27:48 +0300

    монетки
    ф

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public GameObject[] platformPrefabs;
+    public GameObject coin;
     private List<GameObject> _createdPlatforms = new List<GameObject>();
     private float _platformLenght = 144f;
     private float _startPos = 0f;
+    private int _coinCount = 0;
 
     [SerializeField] private Transform _player;
 
@@ -37,7 +39,15 @@ public class PlatformSpawner : MonoBehaviour
     }
     private void SpawnPlatform(int index)
     {
+        _coinCount = Random.Range(1, 3);
         GameObject newPlatform = Instantiate(platformPrefabs[index], transform.forward * _startPos, Quaternion.identity);
+        for (int i = 0; i < _coinCount; i++)
+        {
+            GameObject newCoin = Instantiate(coin);
+            Vector3 newPosition = new Vector3(Random.Range(-10.5f, 10.5f), 0.55f, Random.Range(_startPos - 72, _startPos + 72));
+            newCoin.transform.position = newPosition;
+            newCoin.transform.SetParent(newPlatform.transform);
+        }
         newPlatform.SetActive(true);
         _createdPlatforms.Add(newPlatform);
         _startPos += _platformLenght;
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,16 +5,25 @@ public class Score : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private Text _scoreText;
-    private string text = "SCORE ";
-    void Update()
+    [SerializeField] private Text _coinCountText;
+    private string _scoretext = "SCORE ";
+    private string _coinsText = "x ";
+    private int _coinCount = 0;
+    private void Update()
     {
         if (_player.transform.position.z > 0 )
         {
-            _scoreText.text = text + ((int)(_player.position.z / 2)).ToString();
+            _scoreText.text = _scoretext + ((int)(_player.position.z / 2)).ToString();
         }
         else
         {
-            _scoreText.text = text + 0;
+            _scoreText.text = _scoretext + 0;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        _coinCount++;
+        _coinCountText.text = _coinsText + _coinCount;
+
     }
 }
